c39a16e8de93f346a11654a5ba512e41fb23c912
daryafekt <124902445+daryafekt@users.noreply.github.com>
2023-03-02 16:15:19 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,20 +1,59 @@
-y=0
-for x in range(1,101):
-  if x%15==0:
-    print("FizzBuzz")
-  elif x%3==0:
-     print("Fizz")
-     y=y+1
-  elif x%5==0:
-    print("Buzz")
-  else: print(x)
+# imie="ivgfcycj"
+# if imie:
+#     print("imie istnieje")
+# nazwisko=""
+# if nazwisko:
+#     print("nazwisko istnieje")
+#
+# #false:
+# print(bool(""))
+# #true:
+# print(bool("hyftfduydfuy"))
+#
+# successful = True
+# for number in range(3):
+#     print("attempt")
+#     if successful:
+#         print("successful")
+#         break
+#     else: print("attepted 3 times and failed")
+#
+#
+# for x in range(5):
+#     for y in range(3):
+#         print(f"{x},{y}")
+#
+# #iterable
+# for x in ("Python"):
+#     print(x)
+#
+# for x in [1,2,3,4]:
+#     print(x)
 
-print(x)
-print(y, "liczby podzilne przez 3")
+count=0
+for i in range(0,10):
+    if i%2==0:
+       print(i)
+       count=count+1
+print(count)
 
 
+#tworzenie funkcji:
+def nazwafunkcji():
+    print("ffgdhtgdtgdthy")
+nazwafunkcji()
 
 
+z=0
+def dodawanie():
+    print(int(z)+2)
+    print()
+dodawanie()
+
+
+def dodaj(liczba1, liczba2, liczba3=0):  #liczba3 jest opcjonalna
+    print(liczba1+liczba2)
+dodaj(234, 756865)
 
 
 
