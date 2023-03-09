860c0c537ec390f62679a45ed7bf4f493ac76565
daryafekt <124902445+daryafekt@users.noreply.github.com>
2023-03-09 15:39:26 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,67 +1,16 @@
-# imie="ivgfcycj"
-# if imie:
-#     print("imie istnieje")
-# nazwisko=""
-# if nazwisko:
-#     print("nazwisko istnieje")
-#
-# #false:
-# print(bool(""))
-# #true:
-# print(bool("hyftfduydfuy"))
-#
-# successful = True
-# for number in range(3):
-#     print("attempt")
-#     if successful:
-#         print("successful")
-#         break
-#     else: print("attepted 3 times and failed")
-#
-#
-# for x in range(5):
-#     for y in range(3):
-#         print(f"{x},{y}")
-#
-# #iterable
-# for x in ("Python"):
-#     print(x)
-#
-# for x in [1,2,3,4]:
-#     print(x)
+dd=input("podaj dzień= ")
+mm=input("podaj miesiąc= ")
+rr=input("podaj rok= ")
 
-count=0
-for i in range(0,10):
-    if i%2==0:
-       print(i)
-       count=count+1
-print(count)
+data=dd+"-"+mm+"-"+rr
+
+data2=rr+"-"+dd+"-"+mm
+print(data2)
 
 
-#tworzenie funkcji:
-def nazwafunkcji():
-    print("ffgdhtgdtgdthy")
-nazwafunkcji()
-
-
-z=0
-def dodawanie():
-    print(int(z)+2)
-    print()
-dodawanie()
-
-
-def dodaj(liczba1, liczba2, liczba3=0):  #liczba3 jest opcjonalna
-    print(liczba1+liczba2)
-dodaj(234, 756865)
-
-
-
-
-
-
-
-
-
-
-
+b=input("podaj datę w formacie dd-mm-rr ")
+nowa_data=b.split("-") #lista, podzieli string na wyrazy, oddzielone "-"
+print(nowa_data)
+NOWA_DATA=nowa_data[::-1]
+print(NOWA_DATA)
+print("-".join(NOWA_DATA))
